976e5bbde76ab424a9d1312a5148886b373768a2
Harsh Bhatt <harsh.aby.007@gmail.com>
2024-07-24 17:27:55 +0530

    yo v2

--- wallet-connect/src/Pages/Home.jsx
+++ wallet-connect/src/Pages/Home.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import './Home.css'; // Import the CSS file for styling
 
 const Home = () => {
+  const [showWelcome, setShowWelcome] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowWelcome(false);
+    }, 3000); // Hide the welcome message after 3 seconds
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
   return (
     <div className="home-container">
-      <Navbar />
-      <div className="background-image"></div>
+      <div className={`background-image ${showWelcome ? 'blurred' : ''}`}></div>
+      {showWelcome && (
+        <div className="welcome-text">
+          Welcome to Our Website!
+        </div>
+      )}
+      {!showWelcome && (
+        <div className="navbar-container">
+          <Navbar />
+        </div>
+      )}
       <div className="foreground-image"></div>
     </div>
   );
