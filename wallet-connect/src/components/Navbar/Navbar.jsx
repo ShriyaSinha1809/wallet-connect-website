36947e42e45e4c3613ac4222f33fccc90bfd21eb
Aksh10x <akshathsurwase@gmail.com>
2024-07-27 03:53:42 +0530

    uuniujn

--- wallet-connect/src/components/Navbar/Navbar.jsx
+++ wallet-connect/src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ import WalletConnectModal from './WalletConnectModal'; // Import the modal compo
 
 const Navbar = () => {
   const [isModalOpen, setModalOpen] = useState(false);
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
 
   useEffect(() => {
     const links = document.querySelectorAll(`.${styles.navLinks} a`);
@@ -22,17 +23,33 @@ const Navbar = () => {
     setModalOpen(false);
   };
 
+  
+
   return (
     <div className={styles.navbar}>
       <div className={styles.logo}><h2>LOREM</h2></div>
+      
+      <div className={styles.navLinksSidebar} >
+        <Link to="/trade"><FaExchangeAlt /> Trade</Link>
+        <Link to="#"><FaShoppingCart /> Buy</Link>
+        <Link to="#"><FaMoneyBillWave /> Earn</Link>
+        <Link to="#" className={styles.connectWalletside} onClick={handleOpenModal}>
+          <FaWallet /> Connect Wallet
+        </Link>
+      </div> 
+
       <div className={styles.navLinks}>
+     
+        
         <Link to="/trade"> <FaExchangeAlt /> Trade</Link>
         <Link to="#"> <FaShoppingCart /> Buy</Link>
         <Link to="#"> <FaMoneyBillWave /> Earn</Link>
         <Link to="#" className={styles.connectWallet} onClick={handleOpenModal}>
           <FaWallet /> Connect Wallet
         </Link>
+        <Link><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></Link>
       </div>
+      
       <WalletConnectModal isOpen={isModalOpen} onClose={handleCloseModal} />
     </div>
   );
