98dbe77729b15c1df180e06e2f3dcdfec6c2f71d
AnuragP004 <67072944+AnuragP004@users.noreply.github.com>
2024-07-27 16:51:43 +0530

    logos

--- wallet-connect/src/components/Navbar/WalletConnectModal.jsx
+++ wallet-connect/src/components/Navbar/WalletConnectModal.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import styles from './WalletConnectModal.module.css';
-import ImageHover from './imagehover';
-import './imagehover.css';
 
 const WalletConnectModal = ({ isOpen, onClose }) => {
     useEffect(() => {
@@ -35,7 +33,6 @@ const WalletConnectModal = ({ isOpen, onClose }) => {
             <div className={`${styles.modalContainer} ${isOpen ? styles.open : ''}`}>
                 <div className={styles.content}>
                     <div className={styles.leftside}>
-                        {/*<ImageHover />*/}
                         <button className={styles.logoButton1} onClick={handleLogoClick}>
                             <img src="/MetaMask_Fox.svg" alt="Meta Mask Logo" className={styles.logo} />
                         </button>
@@ -43,26 +40,18 @@ const WalletConnectModal = ({ isOpen, onClose }) => {
                             <img src="/wallet-logo.svg" alt="Wallet Connect Logo" className={styles.logo} />
                         </button>
                         <button className={styles.logoButton2} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
+                            <img src="/bnb-bnb-logo.svg" alt="Binance Logo" className={styles.logo} />
                         </button>
                         <button className={styles.logoButton3} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
+                            <img src="/coinbase-wallet-logo.svg" alt="Coinbase Logo" className={styles.logo} />
                         </button>
                         <button className={styles.logoButton4} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
+                            <img src="/trust-wallet-logo.svg" alt="Trust Wallet Logo" className={styles.logo} />
                         </button>
                         <button className={styles.logoButton5} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
+                            <img src="/okx.svg" alt="OKX Logo" className={styles.logo} />
                         </button>
-                        <button className={styles.logoButton6} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
-                        </button>
-                        <button className={styles.logoButton7} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
-                        </button>
-                        <button className={styles.logoButton8} onClick={handleLogoClick}>
-                            <img src="/wallet-logo.svg" alt="Binance Logo" className={styles.logo} />
-                        </button>
+                        <button className={styles.google} onClick={handleLogoClick}><img src="/gicon.svg"/></button>
                     </div>
                     <div className={styles.rightside}>
                         <h5 className={styles.righttext}>Haven’t got a wallet yet?</h5>
