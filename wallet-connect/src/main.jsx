826792a93b01cf1cd38e873c2fe957f8fb0131da
Harsh Bhatt <harsh.aby.007@gmail.com>
2024-08-05 16:16:00 +0530

    yo v13

--- wallet-connect/src/main.jsx
+++ wallet-connect/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Web3ModalProvider } from '../src/config/Web3ModalProvider.tsx' // Ensure the correct path
-import { CharacterAnimationsProvider } from '../src/models/Contexts/CharcaterAnimation.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Web3ModalProvider } from '../src/config/Web3ModalProvider.tsx'; // Ensure the correct path
+import { CharacterAnimationsProvider } from '../src/models/Contexts/CharcaterAnimation.jsx';
+import './customCursor.css'; // Ensure this path is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Web3ModalProvider>
       <CharacterAnimationsProvider>
-      <App />
+        <App />
       </CharacterAnimationsProvider>
     </Web3ModalProvider>
   </React.StrictMode>
-)
+);
